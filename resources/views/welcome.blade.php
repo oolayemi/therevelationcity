cb7ed7af380c625f3c63755d63548fd0e9deef8b
oolayemi <olayemiolaomo5@gmail.com>
2021-09-17 16:07:44 +0100

    ministries updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -156,9 +156,9 @@
                     <div class="col-md-8 ftco-animate">
                         <div class="text mt-md-5 w-100 text-center">
                             <h2>Transforming Live</h2>
-                            <h1 class="mb-3">Total Surrender to God</h1>
+                            <h1 class="mb-3">Total Surrender to <br> God</h1>
                             <p class="mb-4 pb-3"></p>
-                            <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-2 px-md-4">Become A Volunteer</a></p>
+{{--                            <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-2 px-md-4">Become A Volunteer</a></p>--}}
                         </div>
                     </div>
                 </div>
@@ -173,7 +173,7 @@
                             <h2>Revelation City Church</h2>
                             <h1 class="mb-3">Perfect Church For Imperfect People</h1>
                             <p class="mb-4 pb-3"></p>
-                            <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-2 px-md-4">Become A Volunteer</a></p>
+{{--                            <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-2 px-md-4">Become A Volunteer</a></p>--}}
                         </div>
                     </div>
                 </div>
