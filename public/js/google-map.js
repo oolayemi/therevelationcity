07cb080eaeb98e350050c8f8e3324b3ca1be3f46
oolayemi <olayemiolaomo5@gmail.com>
2021-10-30 21:50:08 +0100

    added pin

--- public/js/google-map.js
+++ public/js/google-map.js
@@ -15,14 +15,14 @@ function init() {
         map: map,
     });
 
-    var addresses = ['#205, 8311 Chappelle Way SW, Edmonton AB, Canada T6W3Y8'];
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({position: latlng, map: map, icon: 'images/loc.png'});
-        });
-    }
+    // var addresses = ['#205, 8311 Chappelle Way SW, Edmonton AB, Canada T6W3Y8'];
+    // for (var x = 0; x < addresses.length; x++) {
+    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function (data) {
+    //         var p = data.results[0].geometry.location
+    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
+    //         new google.maps.Marker({position: latlng, map: map, icon: 'images/loc.png'});
+    //     });
+    // }
 }
 
 google.maps.event.addDomListener(window, 'load', init);
